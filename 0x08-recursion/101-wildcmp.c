50702c4882a94e6a4d277216b5ca0aeef4252ed9
BoazAkunga <boazakunga@gmail.com>
2023-08-03 16:27:08 -0700

    function that compares two strings and returns 1 if the strings can be considered identical, otherwise return 0

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,3 +1,28 @@
 #include "main.h"
+
 /**
- * wildcmp -
+ * wildcmp - compare strings with wildcard support
+ * @s1: pointer to string params
+ * @s2: pointer to string params
+ * return: 1 if the strings match, 0 otherwise
+ */
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 == '\0')
+	{
+		if (*s2 != '\0' && *s2 == '*')
+		{
+			return (wildcmp(s1, s2 + 1));
+		}
+		return (*s2 == '\0');
+	}
+	if (*s2 == '*')
+	{
+		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
+	}
+	else if (*s1 == *s2)
+	{
+		return (wildcmp(s1 + 1, s2 + 1));
+	}
+	return (0);
+}
