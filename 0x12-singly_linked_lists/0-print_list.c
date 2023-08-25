5e7f5e27f03af68ac797ee51d450aea018de96ed
BoazAkunga <boazakunga@gmail.com>
2023-08-25 10:26:19 -0700

    functions in C-linked task

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include "lists.h"
 /**
- * print_list - prints all elements of a list_t list
+ * print_list - prints all elements of a the list
  * @h: pointer to the list_t list
  * Return: number of nodes printed
  */
@@ -9,7 +9,7 @@ size_t print_list(const list_t *h)
 {
 	size_t s = 0;
 
-	for (; h; h = h->next)
+	while (h)
 	{
 	if (!h->str)
 		printf("[0] (nil)\n");
