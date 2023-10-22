289c9e8084ea36382d08dfca17efb708993317f2
BoazAkunga <boazakunga@gmail.com>
2023-10-22 13:23:27 -0700

    0. Print list task solution and edited header file

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,7 +10,7 @@
 */
 size_t print_list(const list_t *h)
 {
-	int count = 0;
+	int node = 0;
 
 	while (h)
 	{
@@ -22,8 +22,8 @@ size_t print_list(const list_t *h)
 		{
 		printf("[%d] %s\n", h->len, h->str);
 		}
-		count++;
+		node++;
 		h = h->next;
 	}
-	return (count);
+	return (node);
 }
