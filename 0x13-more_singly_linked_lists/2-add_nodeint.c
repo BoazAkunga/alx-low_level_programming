48bf5af1ea9a4c7ecc61b05ce40c4dd0f309276b
BoazAkunga <boazakunga@gmail.com>
2023-10-23 16:33:03 -0700

    cleared trailing white spaces for this task

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -4,7 +4,7 @@
  * add_nodeint - Adds new node at the beginning of listint_t list
  * @head: Ptr to 1st node of listint_t list
  * @n: Data to insert in new node
- * Return: Ptr to new node else NULL on failure
+ * Return: Ptr to new node on success else NULL
  */
 
 listint_t *add_nodeint(listint_t **head, const int n)
