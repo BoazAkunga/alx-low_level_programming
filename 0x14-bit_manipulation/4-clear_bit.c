973f73d07f3758b4112af90c88ea613b29b46298
BoazAkunga <boazakunga@gmail.com>
2023-10-26 20:25:50 -0700

    Task 4.100 solution

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,13 +1,13 @@
-4-clear_bit.c
-
 #include "main.h"
 #include <stdlib.h>
+
 /**
- * clear_bit - This prog sets the value of a bit to 0 at a given index
- * @n: parameter
- * @index: index
- * Return: 1 if success, -1 if error
+ * clear_bit - Prog sets the value of bit to 0 at given index
+ * @n: Parameter
+ * @index: Index
+ * Return: 1 (success) else -1 (error)
  */
+
 int clear_bit(unsigned long int *n, unsigned int index)
 {
 	if (index > sizeof(n) * 8)
