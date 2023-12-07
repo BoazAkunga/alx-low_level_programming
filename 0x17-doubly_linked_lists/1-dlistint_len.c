5f9b2d80a0e1f04621a0cd4091282870666b1f6d
BoazAkunga <boazakunga@gmail.com>
2023-12-07 15:54:41 -0800

    Task 1. List length solution corrected

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -11,16 +11,9 @@
 
 size_t dlistint_len(const dlistint_t *h)
 {
-	int count;
+	size_t count = 0;
 
-	count = 0;
-
-	if (h == NULL)
-		return (count);
-	while (h->prev != NULL)
-		h = h->prev;
-
-	while(h != NULL)
+	while (h != NULL)
 	{
 		count++;
 		h = h->next;
